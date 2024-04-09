6dc5f015cec323eccbed74000a41e07c4f32140e
NoahGJAC <2028706@johnabbottcollege.net>
2024-04-09 16:11:35 -0400

    Buzzer class reads buzzer state

--- python/actuators/security/buzzer.py
+++ python/actuators/security/buzzer.py
@@ -1,21 +1,47 @@
 #!/usr/bin/env python3
 
+# Imports
 import seeed_python_reterminal.core as rt
-
 import time
 
-# buzzer permisions require sudo
-try:
-    print("BUZZER ON")
-    # change state
-    rt.buzzer = True
 
-    # read state
-    print(rt.buzzer)
-    time.sleep(1)
+class BuzzerController():
 
-    print("BUZZER OFF")
-    rt.buzzer = False
-    print(rt.buzzer)
-except PermissionError as e:
-    print(f"{e}\nDid you forget permissions?\n$ sudo chmod a+rw /sys/class/leds/usr_buzzer/brightness")
+    def __init__(self) -> None:
+        """Initializes the BuzzerController
+        """
+        pass
+
+    def close(self) -> None:
+        """Sets the buzzer state to False, meant for cleaning up.
+        """
+        rt.buzzer = False
+
+    def read_state(self) -> bool:
+        """Returns true if the buzzer state is truthy, false otherwise.
+
+        Returns:
+            bool: The state of the buzzer.
+        """
+        return rt.buzzer
+    
+
+if __name__ == "__main__":
+    buzzer_controller = BuzzerController()
+    try:
+        while True:
+            print(f"Buzzer is {'on' if buzzer_controller.read_state() else 'off'}")
+            time.sleep(1)
+            # change state
+            rt.buzzer = True
+            
+            print(f"Buzzer is {'on' if buzzer_controller.read_state() else 'off'}")
+            time.sleep(1)
+
+            rt.buzzer = False
+            print(f"Buzzer is {'on' if buzzer_controller.read_state() else 'off'}")
+            time.sleep(1)
+    except PermissionError as e:
+        print(f"{e}\nDid you forget permissions?\n$ sudo chmod a+rw /sys/class/leds/usr_buzzer/brightness")
+    finally:
+        buzzer_controller.close()
