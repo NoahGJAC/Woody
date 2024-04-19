34d92a77cd2bf1f15fac04b4529ba6ae65b81100
NoahGJAC <2028706@johnabbottcollege.net>
2024-04-19 17:26:47 -0400

    Update test to use full 180 degrees

--- python/actuators/security/door_lock.py
+++ python/actuators/security/door_lock.py
@@ -8,7 +8,7 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 try:
 
     factory = PiGPIOFactory()
-    servo = Servo(12, pin_factory=factory)
+    servo = Servo(12, min_pulse_width= 0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
     while True:
         print('MIN')
         servo.min()
