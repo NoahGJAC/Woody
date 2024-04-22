85d85160c371118429230fafe9b442b3bc377694
Cindy-Coulibaly <2126537@johnabbottcollege.net>
2024-04-21 20:34:22 -0400

    formatting done

--- python/sensors/geo_location/vibration.py
+++ python/sensors/geo_location/vibration.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import seeed_python_reterminal.core as rt
 import seeed_python_reterminal.acceleration as rt_accel
-from python.sensors.sensors import ISensor,AReading
+from python.sensors.sensors import ISensor, AReading
 import math
 
+
 class vibration(ISensor):
-    def __init__(self, gpio: int | None, model: str , type: AReading.Type):
+    def __init__(self, gpio: int | None, model: str, type: AReading.Type):
         """Initializes the vibration sensor.
 
         Args:
@@ -19,14 +20,14 @@ class vibration(ISensor):
         # Initialize variables to store the last readings
         self.last_x = self.last_y = self.last_z = None
         self.variant = 19
-    
+
     def read_sensor(self) -> list[AReading]:
         """read all of the vibration level: aka if a vibration accured
 
         Return:
             returns the list of all the different readings
         """
-        readings : list[AReading] = []
+        readings: list[AReading] = []
         for event in self.device.read_loop():
             accelEvent = rt_accel.AccelerationEvent(event)
             if accelEvent.name is not None:
@@ -36,54 +37,68 @@ class vibration(ISensor):
                     self.last_y = accelEvent.value
                 elif accelEvent.name == rt_accel.AccelerationName.Z:
                     self.last_z = accelEvent.value
-                
-                # Calculate pitch and roll angles only if all readings are available
+
+                # Calculate pitch and roll angles only if all readings are
+                # available
                 if self.last_x is not None and self.last_y is not None and self.last_z is not None and self.first_x is None and self.first_y is None and self.first_z is None:
                     self.first_x = self.last_x
                     self.first_y = self.last_y
                     self.first_z = self.last_z
-                
+
                 if self.first_x is not None and self.first_y is not None and self.first_z is not None:
-                    if self._range(self.last_x,self.first_x):
+                    if self._range(self.last_x, self.first_x):
                         self.first_x = self.last_x
-                        readings.append(AReading(AReading.Type.VIBRATION,AReading.Unit.UNITLESS,True))
+                        readings.append(
+                            AReading(
+                                AReading.Type.VIBRATION,
+                                AReading.Unit.UNITLESS,
+                                True))
                     elif self._range(self.last_y, self.first_y):
                         self.first_y = self.last_y
-                        readings.append(AReading(AReading.Type.VIBRATION,AReading.Unit.UNITLESS,True))
-                    elif self._range(self.last_z,self.first_z):
+                        readings.append(
+                            AReading(
+                                AReading.Type.VIBRATION,
+                                AReading.Unit.UNITLESS,
+                                True))
+                    elif self._range(self.last_z, self.first_z):
                         self.first_z = self.last_z
-                        readings.append(AReading(AReading.Type.VIBRATION,AReading.Unit.UNITLESS,True))
+                        readings.append(
+                            AReading(
+                                AReading.Type.VIBRATION,
+                                AReading.Unit.UNITLESS,
+                                True))
                     else:
-                        readings.append(AReading(AReading.Type.VIBRATION,AReading.Unit.UNITLESS,False))
+                        readings.append(
+                            AReading(
+                                AReading.Type.VIBRATION,
+                                AReading.Unit.UNITLESS,
+                                False))
                     break
 
         return readings
-    
-    def _range(self,last_value:float,current_value:float) -> bool:
+
+    def _range(self, last_value: float, current_value: float) -> bool:
         """
         Return:
             return a boolean if the current is not between the buffers then there is vibration
 
         """
-        return last_value-self.variant>= current_value or current_value >= last_value+self.variant
+        return last_value - \
+            self.variant >= current_value or current_value >= last_value + self.variant
 
 
-        
-        
 def main():
-    vib = vibration(None,'Built-in Accelerometer',AReading.Type.VIBRATION)
+    vib = vibration(None, 'Built-in Accelerometer', AReading.Type.VIBRATION)
     try:
         while True:
             readings = vib.read_sensor()
             for reading in readings:
-                    print(f'{reading.reading_type.value}: {reading.value} {reading.reading_unit.value}')
+                print(
+                    f'{reading.reading_type.value}: {reading.value} {reading.reading_unit.value}')
 
     except KeyboardInterrupt:
         print("Exiting...")
 
-    
 
 if __name__ == "__main__":
     main()
-        
-            
