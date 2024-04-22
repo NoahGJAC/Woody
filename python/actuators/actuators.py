f7821c98899e0e70359486d8618cdb1377bc1499
Diana <dianakarpeeva@gmail.com>
2024-04-21 23:40:05 -0400

    init fan.py

---
+++ python/actuators/actuators.py
@@ -0,0 +1,113 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class ACommand(ABC):
+    """Abstract class for actuator command. Can be instantiated directly or inherited.
+    Also defines all possible command types via enums.
+    """
+
+    class Type(str, Enum):
+        """Enum defining types of actuators that can be targets for a command
+        """
+        # Add types as needed
+        FAN_ON_OFF = 'fan-on-off'
+        LIGHT_ON_OFF = 'light-on-off'
+        LIGHT_PULSE = 'light-pulse'
+        BUZZER_ON_OFF = 'buzzer-on-off'
+
+    # Class properties that must be defined in implementation classes
+
+    def __init__(self, target: Type, value: str) -> None:
+        """Constructor for Command abstract class
+
+        :param Type target: Type of command whih associated a command to a type of actuator.
+        :param str value: Value of command to be passed to actuator.
+        """
+        self.target_type = target
+        self.value: str = value
+
+    def __repr__(self) -> str:
+        return f'Command setting {self.target_type} to {self.value}'
+
+
+class IActuator(ABC):
+
+    # Class properties that must be set in constructor of implementation class
+    _current_state: str | bool
+    type: ACommand.Type
+
+    @abstractmethod
+    def __init__(self, gpio: int, type: ACommand.Type,
+                 initial_state: str) -> None:
+        """Constructor for Actuator class. Must define interface's class properties
+
+        :param ACommand.Type type: Type of command the actuator can respond to.
+        :param str initial_state: initializes 'current_state' property of a new actuator.
+        If not passed, actuator implementation is responsible for setting a default value.
+        """
+        pass
+
+    @abstractmethod
+    def validate_command(self, command: ACommand) -> bool:
+        """Validates that a command can be used with the specific actuator.
+
+        :param ACommand command: the command to be validated.
+        :return bool: True if command can be consumed by the actuator.
+        """
+        pass
+
+    @abstractmethod
+    def control_actuator(self, value: str) -> bool:
+        """Sets the actuator to the value passed as argument.
+
+        :param str value: Value used to set the new state of the actuator. Value is parsed inside concrete classes.
+        :return bool: True if the state of the actuator changed, false otherwise.
+        """
+        pass
+
+
+class MockActuator(IActuator):
+    """A class that represents a mock actuator that implements the IActuator interface.
+    """
+
+    def __init__(self, gpio: int, type: ACommand.Type,
+                 initial_state: str) -> None:
+        """Initialize the mock actuator. Set the parameters required by the interface.
+
+        Args:
+            gpio (int): The GPIO pin of the mock actuator.
+            type (ACommand.Type): The type of command the mock actuator responds to.
+            initial_state (str): The initial state of the mock actuator.
+        """
+        self._current_state = initial_state
+        self.type = type
+        return
+
+    def validate_command(self, command: ACommand) -> bool:
+        """Simulates validating a command
+
+        Args:
+            command (ACommand): The command to validate
+
+        Returns:
+            bool: Always returns True.
+        """
+        return True
+
+    def control_actuator(self, value: str) -> bool:
+        """Simulates controlling the mock actuator. Prints the value parameter.
+
+        Args:
+            value (str): The value to control the mock actuator.
+
+        Returns:
+            bool: Always returns True.
+        """
+        print(f"Mock actuator control: {value}")
+        return True
+
+    def clean_up(self):
+        """Simulates any clean up needed on the hardware.
+        """
+        pass
---
+++ python/actuators/plant/fan.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+# Imports
+from python.actuators.actuators import IActuator, ACommand
+from enum import Enum
+from gpiozero import OutputDevice
+from time import sleep
+
+
+class FanState(Enum):
+    ON = "on"
+    OFF = "off"
+
+
+class FanController(IActuator):
+    # A class to control a fan through the reterminal.
+
+    def __init__(
+        self,
+        gpio: int,
+        type: ACommand.Type,
+        initial_state: FanState = FanState.OFF,
+    ) -> None:
+        """
+        Initializes the fan.
+
+        Args:
+            gpio int: The gpio of the fan
+            type (ACommand.Type): The type of command the fan accepts.
+            initial_state (str, optional): The initial state of the fan ('on' or 'off'). Defaults to 'off'.
+        """
+        self._validate_integer(gpio, " GPIO")
+        self.validate_command(initial_state)
+
+        self.gpio = gpio
+        self.type = type
+        self.fan = OutputDevice(pin=gpio)
+        self._current_state = initial_state == FanState.ON
+
+        self.control_actuator(self._current_state)
+
+    def _validate_integer(
+        self, value: int, name: str, min_value: int = 0, max_value: int = None
+    ) -> None:
+        if max_value is not None:
+            if not min_value <= value <= max_value:
+                raise ValueError(
+                    f"{name} value must be between {min_value} and {max_value}"
+                )
+        elif value < min_value:
+            raise ValueError(f"{name} value must be positive")
+
+    def validate_command(self, command: ACommand) -> bool:
+        """Validates a command that can be used with the fan.
+
+        Args:
+            command (ACommand): The command to validate.
+
+        Returns:
+            bool: True if the command is valid, False otherwise.
+        """
+        return (
+            command.target_type == self.type
+            and isinstance(command.value, str)
+            and (command.value.lower() in (FanState.ON.value, FanState.OFF.value))
+        )
+
+    def control_actuator(self, value: str) -> bool:
+        """Controls the fan's state.
+
+        Args:
+            value (str): The new state of the fan, 'on' or 'off'.
+
+        Returns:
+            bool: True if the fan's state changes, False otherwise.
+        """
+        previous_state = self._current_state
+
+        if value.lower() not in (FanState.ON.value, FanState.OFF.value):
+            raise ValueError(f"Invalid argument {value}, must be 'on' or 'off'")
+
+        self.fan.value = 1 if value is FanState.ON else 0
+        self._current_state = value
+
+        return previous_state != self._current_state
+
+    def clean_up(self) -> None:
+        # Sets the fan's state to False, meant for cleaning up.
+        self.fan.value = False
+
+    def read_state(self) -> bool:
+        """
+        Returns true if the fan's state is truthy, false otherwise.
+
+        Returns:
+            bool: The state of the fan.
+        """
+        return self.fan.value
+
+
+if __name__ == "__main__":
+    fan_controller = FanController(gpio=16, type=ACommand.Type.FAN_ON_OFF)
+
+    while True:
+        print(f"Fan is {'on' if fan_controller.read_state() else 'off'}")
+        time.sleep(2)
+
+        fan_controller.control_actuator("on")
+
+        print(f"Fan is {'on' if fan_controller.read_state() else 'off'}")
+        time.sleep(2)
+
+        fan_controller.control_actuator("off")
+
+        print(f"Fan is {'on' if fan_controller.read_state() else 'off'}")
+        time.sleep(2)
+
+    fan_controller.clean_up()
