0bca31a4dbc05987ed399e74d321b172fc956883
Cindy-Coulibaly <98350788+Cindy-Coulibaly@users.noreply.github.com>
2024-04-22 10:32:21 -0400

    Initialize Maui app

---
+++ mobile_app/Woody/Woody/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Woody
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ mobile_app/Woody/Woody/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Woody
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
