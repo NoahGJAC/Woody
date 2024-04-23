246902a641815a97ae13c2c46d3701968e6444a6
Cindy-Coulibaly <98350788+Cindy-Coulibaly@users.noreply.github.com>
2024-04-22 23:55:23 -0400

    created the device controller interface

---
+++ python/controllers/device_controllers.py
@@ -0,0 +1,26 @@
+from time import sleep
+
+
+class IDevice_Controller:
+    def __init__(self) -> None:
+        """
+            Initialize the actuators and sensors
+        """
+
+    def control_actuators(self) -> None:
+        """
+            Control all the actuators
+        """
+
+    def read_sensors(self) -> None:
+        """
+            reads all of the sensors
+        """
+
+    def loop(self):
+        while True:
+            self.control_actuators()
+            self.read_sensors()
+            sleep(2)
+
+
