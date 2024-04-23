38a106545ba42243c24881934df8f5b16482beb1
NoahGJAC <2028706@johnabbottcollege.net>
2024-04-23 18:26:17 -0400

    Refactor controller

--- python/controllers/device_controllers.py
+++ python/controllers/device_controllers.py
@@ -1,26 +1,51 @@
 from time import sleep
+from abc import ABC, abstractmethod
+from python.actuators.actuators import IActuator, ACommand
+from python.sensors.sensors import ISensor, AReading
 
 
-class IDevice_Controller:
+class IDeviceController(ABC):
+    """Interface for all device controllers.
+    """
+
+    # Class properties that must be defined in implementation classes.
+    _sensors: list[ISensor]
+    _actuators: list[IActuator]
+
+    @abstractmethod
     def __init__(self) -> None:
         """
             Initialize the actuators and sensors
         """
+        self._sensors: list[ISensor] = self._initialize_sensors()
+        self._actuators: list[IActuator] = self._initialize_actuators()
 
-    def control_actuators(self) -> None:
+    @abstractmethod
+    def _initialize_sensors(self) -> list[ISensor]:
+        """Initializes all sensors and returns them as a list. Intended to be used in class constructor.
+
+        Returns:
+            list[ISensor]: List of initialized sensors.
         """
-            Control all the actuators
+
+    @abstractmethod
+    def _initialize_actuators(self) -> list[IActuator]:
+        """Initializes all actuators and returns them as a list. Intended to be used in class constructor.
+
+        Returns:
+            list[IActuator]: List of initialized actuators.
         """
 
-    def read_sensors(self) -> None:
+    @abstractmethod
+    def control_actuators(self, commands: list[ACommand]) -> None:
+        """Controls actuators according to a list of commands. Each command is applied to it's respective actuator.
+
+        :param list[ACommand] commands: List of commands to be dispatched to corresponding actuators.
         """
-            reads all of the sensors
+
+    @abstractmethod
+    def read_sensors(self) -> list[AReading]:
+        """Reads data from all initialized sensors.
+
+        :return list[AReading]: a list containing all readings collected from sensors.
         """
-
-    def loop(self):
-        while True:
-            self.control_actuators()
-            self.read_sensors()
-            sleep(2)
-
-
--- python/controllers/geo_location_controller.py
+++ python/controllers/geo_location_controller.py
@@ -1,61 +1,102 @@
 #!/usr/bin/env python
+
+
 from time import sleep
 import pynmea2
-from python.actuators.actuators import ACommand
-from python.controllers.device_controllers import IDevice_Controller
+from python.actuators.actuators import ACommand, IActuator
+from python.sensors.sensors import AReading, ISensor
+from python.controllers.device_controllers import IDeviceController
 from python.sensors.geo_location.gps import GPSSensor
 from python.sensors.geo_location.pitch import PitchSensor
 from python.sensors.geo_location.roll import RollSensor
-from python.sensors.sensors import AReading
 from python.actuators.geo_location.buzzer import BuzzerController
+import colorama
 
 
-class Geo_Location_Controller(IDevice_Controller):
+class Geo_Location_Controller(IDeviceController):
     def __init__(self) -> None:
-        self.roll = RollSensor(None, 'Built-in Accelerometer', AReading.Type.ROLL)
-        self.pitch = PitchSensor(None, 'Built-in Accelerometer', AReading.Type.PITCH)
-        self.gps = GPSSensor(None, 'GPS (Air 530)', AReading.Type.GPS)
-        self.buzzer = BuzzerController(
+        super().__init__()
+    
+    def _initialize_actuators(self) -> list[IActuator]:
+        return [
+            BuzzerController(
         gpio=None,
         command_type=ACommand.Type.BUZZER_ON_OFF,
         model='ReTerminal Buzzer',
         reading_type=AReading.Type.BUZZER,
         initial_state='off')
+        ]
+    
+    def _initialize_sensors(self) -> list[ISensor]:
+        return [
+            RollSensor(gpio=None,model='Built-in Accelerometer',type=AReading.Type.ROLL),
+            PitchSensor(gpio=None, model='Built-in Accelerometer',type=AReading.Type.PITCH),
+            GPSSensor(gpio=None, model='GPS (Air 530)',type=AReading.Type.GPS),
+            BuzzerController(
+            gpio=None,
+            command_type=ACommand.Type.BUZZER_ON_OFF,
+            model='ReTerminal Buzzer',
+            reading_type=AReading.Type.BUZZER,
+            initial_state='off')
+        ]
+    
 
 
-    def control_actuators(self) -> None:
-        self._print_readings(self.buzzer.read_sensor())
-        sleep(1)
-        # change state
-        self.buzzer.control_actuator('on')
+    def control_actuators(self, commands: list[ACommand]) -> None:
+        actuator_dict = self._get_actuator_dict()
+        for command in commands:
+            actuator = actuator_dict.get(command.target_type)
+            if actuator is None:
+                print(
+                    colorama.Fore.RED +
+                    f"No actuator found for command: {command}" +
+                    colorama.Fore.RESET
+                )
+                continue
 
-        self._print_readings(self.buzzer.read_sensor())
-        sleep(1)
+            if not actuator.validate_command(command=command):
+                print(
+                    colorama.Fore.RED +
+                    f"Invalid command for actuator: {actuator.type}\n\tCommand: {command}" +
+                    colorama.Fore.RESET
+                )
+                continue
 
-        self.buzzer.control_actuator('off')
-        self._print_readings(self.buzzer.read_sensor())
-        sleep(1)
+            actuator.control_actuator(value=command.value)
+            print(
+                f"Executed command: {command}"
+            )
 
-    def read_sensors(self) -> None:
-        self._print_readings(self.roll.read_sensor())
-        self._print_readings(self.pitch.read_sensor())
-        self._print_readings(self.gps.read_sensor())
+    def _get_actuator_dict(self) -> dict[ACommand.Type, IActuator]:
+        return {actuator.type: actuator for actuator in self._actuators}
+    
+    def read_sensors(self) -> list[AReading]:
+        readings: list[AReading] = [reading for sensor in self._sensors for reading in sensor.read_sensor()]
+        return readings
 
     def loop(self):
+        pre_commands: list[ACommand] = [
+            ACommand(target=ACommand.Type.BUZZER_ON_OFF, value='on')
+        ]
+        post_commands: list[ACommand] = [
+            ACommand(target=ACommand.Type.BUZZER_ON_OFF, value='off')
+        ]
         while True:
-            self.control_actuators()
-            self.read_sensors()
+            self.control_actuators(commands=pre_commands)
+            readings = self.read_sensors()
+            for reading in readings:
+                print(reading)
             sleep(2)
-
-    def _print_readings(self,readings: list[AReading]) -> None:
-        for reading in readings:
-            print(reading)
+            self.control_actuators(commands=post_commands)
+            readings = self.read_sensors()
+            for reading in readings:
+                print(reading)
+            sleep(2)
 
 
 def main():
     controller = Geo_Location_Controller()
     controller.loop()
-
 
 
 if __name__ == "__main__":
