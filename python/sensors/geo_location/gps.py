6142ffda4fa0cb5a6acdf84c13781c8b29e0fc6e
Cindy-Coulibaly <2126537@johnabbottcollege.net>
2024-04-23 18:44:20 -0400

    fixed the gps return for the controller

--- python/sensors/geo_location/gps.py
+++ python/sensors/geo_location/gps.py
@@ -54,6 +54,7 @@ class GPSSensor(ISensor):
         except pynmea2.ParseError as e:
             print(
                 f"{e} \nCould not parse the information? you need to plug the GPS on UART port. Also make sure that your GPS is near an open window and replug your GPS after restarting your Raspberri Pi.")
+            return readings
             pass
 
     def close(self) -> None:
