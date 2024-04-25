c299941fc34336f1af2fb063c51a3afe78a8b5e5
Noah Groleau <98351050+NoahGJAC@users.noreply.github.com>
2024-04-25 19:53:29 -0400

    Create SecurityController.cs

---
+++ mobile_app/Woody/Woody/Models/SecurityController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Woody.Interfaces;
+
+namespace Woody.Models
+{
+    public class SecurityController : INotifyPropertyChanged, IController
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<ISensor> _sensors;
+        public List<ISensor> Sensors
+        {
+            get => _sensors;
+        }
+
+        private List<IActuator> _actuators;
+        public List<IActuator> Actuators
+        {
+            get => _actuators;
+        }
+
+        // Don't think the models should have methods like this idk..
+        public void ControlActuators(List<ICommand> commands)
+        {
+            
+        }
+
+        public List<IReading> ReadSensors()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
