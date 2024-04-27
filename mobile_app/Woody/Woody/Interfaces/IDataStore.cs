aa76da59b35698d3d3b32c9465d73bcf82f40e6a
Noah Groleau <98351050+NoahGJAC@users.noreply.github.com>
2024-04-27 10:00:47 -0400

    add empty db services, start Security DataRepo

---
+++ mobile_app/Woody/Woody/Interfaces/IDataStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Woody.Interfaces
+{
+    internal interface IDataStore<T>
+    {
+        /* taken from lab6
+        Task<bool> AddItemsAsync(T item);
+        Task<bool> UpdateItemsAsync(T item);
+        Task<bool> DeleteItemsAsync(T item);
+        Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+        public ObservableCollection<T> Items { get; }
+        */
+    }
+}
---
+++ mobile_app/Woody/Woody/Services/ContainerDatabaseService.cs
@@ -0,0 +1,15 @@
+﻿using ClassKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Woody.Interfaces;
+
+namespace Woody.Services
+{
+    // ContainerDatabaseService will most likely use CosmosDB, while UserDatabaseService will use firebase
+    public class ContainerDatabaseService<T> : IDataStore<T> where T : class, IController
+    {
+    }
+}
