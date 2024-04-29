eff5928a70a06f65bff4f35ccbc0c48ba8c9331a
Diana Karpeev <2059788@johnabbottcollege.net>
2024-04-29 19:01:21 -0400

    plant page

--- mobile_app/Woody/Woody/App.xaml.cs
+++ mobile_app/Woody/Woody/App.xaml.cs
@@ -5,14 +5,19 @@ namespace Woody
 {
     public partial class App : Application
     {
+        private static PlantRepo plantRepo;
         private static SecurityRepo securityRepo;
+
+        public static PlantRepo PlantRepo
+        {
+            get { return plantRepo ??= new PlantRepo(); }
+        }
+
         public static SecurityRepo SecurityRepo
         {
-            get
-            {
-                return securityRepo ??= new SecurityRepo();
-            }
+            get { return securityRepo ??= new SecurityRepo(); }
         }
+
         public App()
         {
             InitializeComponent();
