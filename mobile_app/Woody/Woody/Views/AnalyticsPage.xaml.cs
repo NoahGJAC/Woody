4a0406bfd30f2351e4a7c0d4d8f7510a59b71098
Noah Groleau <98351050+NoahGJAC@users.noreply.github.com>
2024-04-29 20:35:43 -0400

    add analytics, TODO: replace plant charts

--- mobile_app/Woody/Woody/Views/AnalyticsPage.xaml.cs
+++ mobile_app/Woody/Woody/Views/AnalyticsPage.xaml.cs
@@ -1,9 +1,47 @@
+using LiveChartsCore.SkiaSharpView.Maui;
+using System.Collections.ObjectModel;
+using Woody.DataRepos;
+
 namespace Woody.Views;
 
 public partial class AnalyticsPage : ContentPage
 {
-	public AnalyticsPage()
+    /// <summary>
+    /// Gets or sets the collection of security Cartesian charts displayed on the page.
+    /// </summary>
+    public ObservableCollection<CartesianChart> ChartsSecurity { get; set; }
+
+    /// <summary>
+    /// Gets or sets the collection of plant Cartesian charts displayed on the page.
+    /// </summary>
+    public ObservableCollection<CartesianChart> ChartsPlant { get; set; }
+    public AnalyticsPage()
 	{
 		InitializeComponent();
+
+        ChartsSecurity = new ObservableCollection<CartesianChart>
+        {
+            ChartsRepo.GetNoiseChart(App.SecurityRepo.NoiseLevels),
+            ChartsRepo.GetLuminosityChart(App.SecurityRepo.LuminosityLevels)
+        };
+        
+        // TODO: REMOVE THIS
+        ChartsPlant = new ObservableCollection<CartesianChart>
+        {
+            ChartsRepo.GetNoiseChart(App.SecurityRepo.NoiseLevels),
+            ChartsRepo.GetLuminosityChart(App.SecurityRepo.LuminosityLevels)
+        };
+
+
+        //ChartsPlant = new ObservableCollection<CartesianChart>
+        //{
+        //    ChartsRepo.GetTemperatureChart(App.PlantRepo.TemperatureLevels),
+        //    ChartsRepo.GetHumidityChart(App.PlantRepo.HumidityLevels)
+        //};
+        ChartCarouselSecurity.BindingContext = this;
+        IndicatorViewSecurity.BindingContext = this;
+
+        ChartCarouselPlant.BindingContext = this;
+        IndicatorViewPlant.BindingContext = this;
 	}
 }
